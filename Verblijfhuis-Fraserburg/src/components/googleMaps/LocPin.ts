1b0b6d76e19261bc51f1737a6dc1cdae676045b5
Coffee Coder Sa <bianca.kleynhans93@gmail.com>
2022-11-09 09:32:05 +0200

    Cleaned up

--- Verblijfhuis-Fraserburg/src/components/googleMaps/LocPin.ts
+++
@@ -1,7 +0,0 @@
-// export const LOCPIN: Coords = { lat: 32.143796, lng: 21.904448 };
-export const LOCPIN: Coords = { lat: -31.883512581251008, lng: 21.54678718220334 };
-
-interface Coords {
-  lat: number;
-  lng: number;
-}
